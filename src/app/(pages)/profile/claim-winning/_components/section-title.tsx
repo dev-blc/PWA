e7f56b5ccf828edc0383be1d579c0252497e869a
eldante <dan.tgw@gmail.com>
2025-02-08 15:54:05 +0800

    Added pool row details. (#134)
    * Added pool row details.
    
    * Shorten winning amount if too small a number.
    
    ---------
    
    Co-authored-by: Otto G <5030059+ottodevs@users.noreply.github.com>

--- src/app/(pages)/profile/claim-winning/_components/section-title.tsx
+++ src/app/(pages)/profile/claim-winning/_components/section-title.tsx
@@ -1,7 +1,7 @@
 const SectionTitle = () => {
     return (
         <div className='flex w-full items-start border-b border-[#EDEDED] pb-2'>
-            <div className='text-[11px] font-semibold'>Claimable</div>
+            <div className='text-[11px] font-semibold'>Claimable Winnings</div>
         </div>
     )
 }
