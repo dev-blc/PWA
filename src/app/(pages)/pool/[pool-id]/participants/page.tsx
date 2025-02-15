2a635a8a972b09232a9073176aa912cca65eb978
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-02-15 16:53:35 +0700

    fixup! chore: adjust breaking changes from next15

--- src/app/(pages)/pool/[pool-id]/participants/page.tsx
+++ src/app/(pages)/pool/[pool-id]/participants/page.tsx
@@ -2,9 +2,10 @@ import PageWrapper from '@/components/page-wrapper'
 import { getUserAdminStatusActionWithCookie } from '@/features/users/actions'
 import Participants from './_components/participants'
 
-type Props = { params: { 'pool-id': string } }
+type Props = { params: Promise<{ 'pool-id': string }> }
 
-export default async function ManageParticipantsPage({ params: { 'pool-id': poolId } }: Props) {
+export default async function ManageParticipantsPage({ params }: Props) {
+    const { 'pool-id': poolId } = await params
     const isAdmin = await getUserAdminStatusActionWithCookie()
 
     return (
