3cba55c96f2ec58133e7a5444e75e0f83b8d4e90
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-02-15 16:59:41 +0700

    chore: remove old unused okx widget component

--- src/app/(pages)/profile/cross-swap/page.tsx
+++ src/app/(pages)/profile/cross-swap/page.tsx
@@ -1,5 +1,4 @@
 import PageWrapper from '@/components/page-wrapper'
-import CrossSwapSection from './_components/cross-swap-section'
 import CrossChainSwapSection from './_components/cross-chain-swap-section'
 
 export default function ProfileLayout() {
@@ -10,7 +9,6 @@ export default function ProfileLayout() {
                 title: 'swap',
             }}>
             <div className='flex flex-1 flex-col gap-3'>
-                {/* <CrossSwapSection/>  // UN COMMENT THIS AND COMMENT `CrossChainSwapSection` TO ENABLE OKX WIDGET INSTEAD OF OKX API */}
                 <CrossChainSwapSection />
             </div>
         </PageWrapper>
